44e5e2527434fe9a468ec3e33ebeacf9b81d490a
Neeraj Kashyap <nkashy1@gmail.com>
2021-07-10 11:37:45 -0700

    Superficial CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::{AppSettings, Clap};
+
+const BUZZ_VERSION: &str = "0.0.1";
+const BUZZ_AUTHOR: &str = "Bugout (engineering@bugout.dev)";
+
+/// Runs a given set of patterns on a stream of tags and only prints the matches
+#[derive(Clap)]
+#[clap(version = BUZZ_VERSION, author = BUZZ_AUTHOR)]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Args {
+    /// Path to file containing buzz patterns to match in the input stream
+    #[clap(short, long)]
+    patterns: String,
+    tags: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = Args::parse();
+    println!("Patterns: {}, tags: {:?}", args.patterns, args.tags);
 }
